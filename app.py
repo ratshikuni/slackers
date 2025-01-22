bc255baffc70c21baac68c4b44f74a19a6c202c7
pfunzo <pfunzo@synthesis.co.za>
2025-01-23 01:06:14 +0200

    chore : added Firebase config and logic for Page navigation, added Home page

--- app.py
+++ app.py
@@ -2,102 +2,162 @@ import logging
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from forms import RegistrationForm, LoginForm
+from flask import session
 from werkzeug.security import generate_password_hash, check_password_hash
+import firebase_admin
+from firebase_admin import credentials, auth, firestore
+from datetime import datetime
+import os
 
-# Initialize the Flask app
+
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Replace with a strong secret key
+app.secret_key = 'your_secret_key'  
 
-# Configure the SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
+cred_path = "key.json"
 
-# User Model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
 
-# User Registration Form
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    role = SelectField('Role', choices=[('student', 'Student'), ('teacher', 'Teacher')], validators=[DataRequired()])
-    submit = SubmitField('Sign Up')
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = cred_path
 
-    def validate_email(self, email):
-        if User.query.filter_by(email=email.data).first():
-            raise ValidationError('Email already exists. Please use a different one.')
+cred = credentials.Certificate(cred_path)
+firebase_admin.initialize_app(cred)
 
-    def validate_username(self, username):
-        if User.query.filter_by(username=username.data).first():
-            raise ValidationError('Username already exists. Please choose a different one.')
+db = firestore.client()
 
-# User Login Form
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
 
-# Create database tables
-with app.app_context():
-    db.create_all()
+@app.context_processor
+def inject_user():
+    return dict(logged_In=True)
 
 @app.route('/')
-def index():
+def index(): 
     return render_template('index.html')
+
+@app.route('/home')
+def home():
+
+    if session.get("is_logged_in", False):
+        return render_template('home.html', name=session["email"])  # Subject page
+    return redirect(url_for('login'))
+
+@app.route('/gallery')
+def gallery():
+    print(session)
+
+    if session.get("is_logged_in", False):
+        return render_template('gallery.html') 
+        
+    return redirect(url_for('login'))
+
+
+@app.route('/subject')
+def subject():
+    print(session)
+
+    if session.get("is_logged_in", False):
+        return render_template('subject.html')  
+    return redirect(url_for('login'))
+
+@app.route('/live_lesson')
+def live_lesson():
+    if session.get("is_logged_in", False):
+        return render_template('live_lesson.html')  
+    return redirect(url_for("login"))
+
+@app.route('/logout')
+def logout():
+
+    # db.child("users").child(session["uid"]).update({"last_logged_out": datetime.now().strftime("%m/%d/%Y, %H:%M:%S")})
+    print(session)
+    session["is_logged_in"] = False
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        role = form.role.data
-        # Add your registration processing logic here
-        return redirect(url_for('login'))
+    if request.method == 'POST':
+        if form.is_submitted():
+            email = form.email.data
+            name = form.name.data
+            surname = form.surname.data
+            password = form.password.data
+            user = auth.create_user(email=email, password=password)
+       
+        
+            print(user)
+            session["uid"] = user.uid
+            session["name"] = name
+            session["surname"] = surname
+            session["email"] = email
+            session["is_logged_in"] = True
+            # Save user data
+            data = {
+                "name": name,
+                "email": email,
+                "surname" : surname,
+                "last_logged_in": datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+            }
+            
+            try : 
+                db.collection("users").document(session["uid"]).set(data)
+ 
+                return redirect(url_for('home'))
+            except Exception as e:
+                flash(f'Login successful!{e}', 'success')
+                
+
+
     return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        # Add your login processing logic here
-        return redirect(url_for('gallery_page'))
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+
+
+            
+            try : 
+                user = auth.get_user_by_email(email)
+
+                print(user.uid)
+                # if user.password ==
+                # print(user.password)
+
+
+                session["is_logged_in"] = True
+                session['uid'] = user.uid
+                session["email"] = email
+                
+                # doc_ref = db.collection('users').document(user.uid)
+                # doc = doc_ref.get()
+                flash('Login successful!', 'success')
+                return redirect(url_for('home'))
+            except Exception as e:
+                flash(f'Login successful!{e}', 'success')
+                
+     
+
     return render_template('login.html', form=form)
 
-@app.route('/logout')
-def logout():
-    # session.pop('user_id', None)
-    # session.pop('username', None)
-    # flash('You have been logged out.', 'info')
-    return redirect(url_for('index'))
+# Route for password reset
+@app.route("/reset_password", methods=["GET", "POST"])
+def reset_password():
+    if request.method == "POST":
+        email = request.form["email"]
+        try:
+            
+            auth.send_password_reset_email(email)
+            return render_template("reset_password_done.html") 
+        except Exception as e:
+            print("Error occurred: ", e)
+            return render_template("reset_password.html", error="An error occurred. Please try again.")  
+    else:
+        return render_template("reset_password.html") 
 
-@app.route('/gallery')
-def gallery_page():
-    return render_template('gallery.html')  # Gallery page
-
-@app.route('/subject')
-def subject():
-    return render_template('subject.html')  # Subject page
-
-@app.route('/live_lesson')
-def live_lesson():
-    try:
-        return render_template('live_lesson.html')  # Render the live lesson page template
-    except Exception as e:
-        app.logger.error(f"Error in live_lesson route: {e}")
-        return "An internal error occurred.", 500
 
 @app.route('/recorded-lesson')
 def recorded_lesson():
