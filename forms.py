07cb1f236dc2c61211272288d2300266cc435eb3
pfunzo <pfunzo@synthesis.co.za>
2025-01-23 01:01:03 +0200

    refactor : added Form classes for easier management

--- forms.py
+++ forms.py
@@ -1,12 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField(
+        'Confirm Password', validators=[DataRequired(), EqualTo('password')]
+    )
+    role = SelectField(
+        'Role', choices=[('student', 'Student'), ('teacher', 'Teacher')], validators=[DataRequired()]
+    )
+    submit = SubmitField('Sign Up')
+
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
 
-class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    role = SelectField('Register as', choices=[('learner', 'Learner'), ('teacher', 'Teacher')], validators=[DataRequired()])
+    
+    submit = SubmitField('Login')
