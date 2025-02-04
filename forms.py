377be099f04d0f86ee0577c62d30e4e0fd35b2f0
pfunzo <pfunzo@synthesis.co.za>
2025-02-04 17:56:15 +0200

    chore :  removed unused form objects && setting gunicorn config

--- forms.py
+++ forms.py
@@ -2,26 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, FileField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
-class RegistrationForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    surname = StringField('Surname', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField(
-        'Confirm Password', validators=[DataRequired(), EqualTo('password')]
-    )
-    role = SelectField(
-        'Role', choices=[('student', 'Student'), ('teacher', 'Teacher')], validators=[DataRequired()]
-    )
-    submit = SubmitField('Sign Up')
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-
-    
-    submit = SubmitField('Login')
-
 class TutorApplicationForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
